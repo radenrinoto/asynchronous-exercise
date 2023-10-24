4809ef76ef9e4839efb137dbf656ab34baf9b988
fadilmuiz <fadilmuiz1319@gmail.com>
2023-10-24 11:24:23 +0700

    phase 2 respond

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-async function data() {
+// example Response
+// async function data() {
+//   try {
+//     const response = await fetch("https://restcountries.com/v3.1/region/europe")
+//     const allData = await response.json();
+//     const centralEurope = allData.filter(
+//       (dataOfCountry) => dataOfCountry.subregion === "Central Europe"
+//     );
+//     const data25Country = centralEurope.slice(0, 25);
+//     const result = data25Country.map(
+//       ({name, status, subregion, population, languages, capital}) => ({
+//         name, status, subregion, population, languages, capital
+//       })
+//     );
+//     console.log("Success bre >>>", result);
+//   } catch (error) {
+//     console.log("Error bre >>>", error);
+//   }
+// }
+// data()
+
+// response phase 2
+
+async function response2() {
   try {
     const response = await fetch("https://restcountries.com/v3.1/region/europe")
     const allData = await response.json();
-    const centralEurope = allData.filter(
+    const centralEuropeCountry = allData.filter(
       (dataOfCountry) => dataOfCountry.subregion === "Central Europe"
     );
-    const data25Country = centralEurope.slice(0, 25);
+    const data25Country = centralEuropeCountry.slice(0, 25);
     const result = data25Country.map(
       ({name, status, subregion, population, languages, capital}) => ({
         name, status, subregion, population, languages, capital
       })
     );
-    console.log("Success bre >>>", result);
+    let obj = {centralEurope: {result}}
+    console.log("Success", obj); 
   } catch (error) {
-    console.log("Error bre >>>", error);
+    console.log("Error", error);
   }
 }
-data()
+
+response2()
