ce969882d68d6d91d6592956d9e26c469c261725
Adam Sake Arfansyah <adamsake8@gmail.com>
2023-10-24 15:56:35 +0700

    add: exercise 3

---
+++ exercise3/jsonplaceholder/index.js
@@ -0,0 +1,55 @@
+const URL_JSONPLACE = "https://jsonplaceholder.typicode.com";
+
+const callApi = async (params, method = "GET", body) => {
+  try {
+    const res = await fetch(`${URL_JSONPLACE}${params}`, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getDataUsers = async () => {
+  const getUser = await callApi("/users");
+  console.log({ getUser });
+  return data;
+};
+
+const postDataPosts = async (data) => {
+  const user = await getDataUsers();
+  const dataRes = {
+    userId: user[0].id,
+    ...data,
+  };
+
+  const res = await callApi("/posts", "POST", dataRes);
+  console.log({ res });
+  return res;
+};
+
+postDataPosts({ title: "TEST", body: "WPLAWPLW" });
+
+const deleteDataPost = async (id) => {
+  const resDelete = await callApi(`/posts/${id}`, "DELETE");
+  console.log({ resDelete });
+  return resDelete;
+};
+
+deleteDataPost(1);
+
+const updateDataPost = async (data) => {
+  const { id, ...rest } = data;
+  const resPatch = await callApi(`/posts/${id}`, "PATCH", rest);
+  console.log({ resPatch });
+  return resPatch;
+};
+
+updateDataPost({ id: 1, title: "UPDATE", body: "UPDATEEE", userId: 1 });
---
+++ exercise3/reqres/index.js
@@ -0,0 +1,41 @@
+const URL = "https://reqres.in/api";
+
+const callApi = async (params, method = "GET", body) => {
+  try {
+    const res = await fetch(`${URL}${params}`, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllUsers = async () => {
+  const users = await callApi(`/users`);
+  console.log(users.data);
+  return users.data;
+};
+
+getAllUsers();
+
+const loginUser = async (data) => {
+  const loginUser = await callApi(`/login`, "POST", data);
+  const token = loginUser.token;
+
+  let fakeCookie = {};
+  if (token) {
+    fakeCookie = token;
+  }
+
+  console.log({ fakeCookie });
+  return fakeCookie;
+};
+
+loginUser({ email: "eve.holt@reqres.in", password: "string" });
