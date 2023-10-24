243059f2b9297cf56936b5fb39da77bd10807c7d
Daniel Riyanto <danielriyanto190700@gmail.com>
2023-10-24 10:38:23 +0700

    Complete the asynchronous exercise

---
+++ index.js
@@ -0,0 +1,16 @@
+async function getCountries() {
+  const response = await fetch("https://restcountries.com/v3.1/region/europe");
+  const countries = await response.json();
+  const first25Countries = countries.slice(0, 25);
+  const centralEuropeCountries = first25Countries.filter(
+    (country) => country.subregion == "Central Europe"
+  );
+  const result = centralEuropeCountries.map(
+    ({name, status, subregion, population, languages, capital}) => ({
+      name, status, subregion, population, languages, capital
+    })
+  );
+  console.log(result);
+}
+
+getCountries();
