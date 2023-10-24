960e0044f8623b553507a446f9b7cc6cf618d39e
andika-hiro <hiroandika@gmail.com>
2023-10-24 11:46:10 +0700

    penambahan exercise 2

---
+++ exercise2.js
@@ -0,0 +1,43 @@
+const url = "https://restcountries.com/v3.1/region/europe?_limit=10";
+
+const restCountries = async () => {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    const limitData = result.slice(0, 25);
+    const europeFilter = limitData.filter((e) => e.subregion === "Central Europe");
+    const southFilter = limitData.filter((e) => e.subregion === "Southeast Europe");
+    const northernFilter = limitData.filter((e) => e.subregion === "Northern Europe");
+    const columnData = {
+      centralEurope: europeFilter.map((x) => ({
+        name: x.name,
+        status: x.status,
+        subregion: x.subregion,
+        population: x.population,
+        language: x.language,
+        capital: x.capital,
+      })),
+      southEast: southFilter.map((x) => ({
+        name: x.name,
+        status: x.status,
+        subregion: x.subregion,
+        population: x.population,
+        language: x.language,
+        capital: x.capital,
+      })),
+      northernEurope: northernFilter.map((x) => ({
+        name: x.name,
+        status: x.status,
+        subregion: x.subregion,
+        population: x.population,
+        language: x.language,
+        capital: x.capital,
+      })),
+    };
+    console.log(columnData);
+  } catch (error) {
+    console.log("Fetch error: ", error);
+  }
+};
+
+restCountries();
